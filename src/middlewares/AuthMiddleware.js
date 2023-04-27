a2c6cf63cbd71662648c5e806fae564051cff268
BenQ <nghiavan0610@gmail.com>
2023-04-27 18:22:41 +0700

    Update AuthMiddleware: using access/refresh Token

--- src/middlewares/AuthMiddleware.js
+++ src/middlewares/AuthMiddleware.js
@@ -1,34 +1,43 @@
-const config = require('../config/env');
 const { ApiError } = require('../helpers/ErrorHandler');
-const { jwtr } = require('../helpers/Token');
+const { verifyAccessToken } = require('../helpers/Token');
+const redisClient = require('../configs/init.redis');
 const passport = require('passport');
-
 const { User } = require('../db/models');
 
-const requireAuth = async (req, res, next) => {
+const authenticateToken = async (req, res, next) => {
     try {
         if (req.isAuthenticated()) {
             return next();
         }
-        // if (!req.cookies.jwt) {
-        //     throw new ApiError(401, 'Token not found');
-        // }
         const { authorization } = req.headers;
 
         if (!authorization || !authorization.startsWith('Bearer')) {
             throw new ApiError(401, 'Invalid authorization');
         }
 
-        const token = authorization.split(' ')[1];
+        const accessToken = authorization.split(' ')[1];
+        if (!accessToken) {
+            throw new ApiError(401, 'Access Token was not found');
+        }
 
-        const payload = await jwtr.verify(token, config.JWT_SECRET);
-        const user = await User.findByPk(payload.id);
-        req.jti = payload.jti;
+        const payload = await verifyAccessToken(accessToken);
+
+        const user = await User.findByPk(payload.userId, { attributes: ['id', 'name', 'avatarUrl', 'role', 'slug'] });
+        if (!user) {
+            throw new ApiError(401, `Account does not exist`);
+        }
+        const storedAccessToken = await redisClient.get(`accessToken:${user.id}`);
+        if (accessToken !== storedAccessToken) {
+            throw new ApiError(401, 'Access token has been revoked');
+        }
         req.user = user;
         next();
     } catch (err) {
+        if (err.name === 'TokenExpiredError') {
+            next(new ApiError(401, 'Access token has been time out'));
+        }
         next(err);
     }
 };
 
-module.exports = { requireAuth };
+module.exports = { authenticateToken };
