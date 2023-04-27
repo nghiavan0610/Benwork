439c04c09413ead608b180ef9276bcc288126b1d
BenQ <nghiavan0610@gmail.com>
2023-04-27 18:35:36 +0700

    Implement V1 routes

--- src/routes/search.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const searchController = require('../app/controllers//SearchController');
-const { requireAuth } = require('../middlewares/AuthMiddleware');
-
-router.get('/gigs', searchController.searchGigs);
-router.get('/users', searchController.searchUsers);
-
-module.exports = router;
